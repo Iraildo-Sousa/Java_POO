aaed3de7c64d32315fd9bbe23db5c3f35065959f
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2024-11-22 12:58:02 -0300

    Add files via upload

---
+++ Terceiro_projeto_classes/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+ 
+    public static void main(String[] args) {
+
+        /* objeto 1 */
+        ninja naruto = new ninja();
+        naruto.nome = "Naruto Uzumaki";
+        
+        /* objeto 2 */
+        ninja sasuke = new ninja();
+        sasuke.nome = "Sasuke Uchiha";
+        sasuke.sharinganAtivado();
+
+    }//public static void main(String[] args) 
+
+}//public class Main
---
+++ Terceiro_projeto_classes/src/ninja.java
@@ -0,0 +1,14 @@
+public class ninja {
+
+    String nome;
+    String aldeia;
+    int idade;
+
+    public void sharinganAtivado() {
+
+        System.out.println("Sharingan ativado!");
+
+    }/* public void sharinganAtivado()  */
+
+
+}//public class ninja
